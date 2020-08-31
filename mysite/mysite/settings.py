49ba81243bba3b324c8e9574a2458d838b5a004f
Shikha Lakra <slakra567@gmail.com>
2020-08-31 19:08:02 +0530

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'blog/templates/blog')
 SECRET_KEY = '=@mgxkqw74!kb6e%jevk64!2=j%a%g_0m%*ky^n5#(ei*oe4cw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blogista.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
